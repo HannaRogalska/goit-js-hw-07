dd21f9aa4244140a28e7dc0d5c3fbf7cdae8a6e1
Hanna Rogalska <hanrogalska@gmail.com>
2023-02-24 09:02:41 +0100

    task 1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,30 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import { galleryItems } from "./gallery-items.js";
 
-console.log(galleryItems);
+
+const getContainGallary = document.querySelector(".gallery");
+console.log(getContainGallary);
+const setNewItemsElements = galleryItems
+  .map((elem) => { return `<div class="gallery__item">
+    <a class="gallery__link" href="${elem.original}">
+    <img class="gallery__image"
+    src="${elem.preview}"
+    data-source="${elem.original}"
+    alt="${elem.description}"/>
+    </div>`;
+  })
+  .join(" ");
+console.log(setNewItemsElements);
+getContainGallary.insertAdjacentHTML("beforeend", setNewItemsElements);
+getContainGallary.style.display = "flex";
+
+
+
+const containerClick = (e) => {
+    e.preventDefault();
+    const source = e.target.dataset.source;
+    const instance = basicLightbox.create(`
+    <img src="${source}" width="800" height="600">`);
+
+    instance.show();
+};
+ getContainGallary.addEventListener("click", containerClick);
